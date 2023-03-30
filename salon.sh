01f34064078c150e7a46d6e3a4673a7a31ea011c
Mark Dexter <a.markdexter@gmail.com>
2023-03-30 23:06:46 +0800

    Update salon.sh

--- salon.sh
+++ salon.sh
@@ -2,40 +2,51 @@
 
 PSQL="psql -X --username=freecodecamp --dbname=salon --tuples-only -c"
 
-echo -e "\n~~~~ MY SALON ~~~~\n"
+echo -e "\n~~~~~ MY SALON ~~~~~\n"
 
 MAIN_MENU() {
 
-  echo -e "\nWelcome to My Salon, how can I help you?\n"
+  echo -e "\nWelcome to My Salon, how can I help you\n"
 
-  SERVICES_MENU=$($PSQL "SELECT * FROM services")
+  # get the services
 
-  echo "$SERVICES_MENU" | while read SERVICE_ID BAR NAME
+  SERVICES=$($PSQL "SELECT * FROM services")
+
+  echo "$SERVICES" | while read SERVICE_ID BAR SERVICE_NAME
   do
-    echo "$SERVICE_ID) $NAME"
-  done  
+    echo -e "$SERVICE_ID) $SERVICE_NAME"
+  done
 
-  echo -e "\nChoose service to be scheduled"
+  # pick a service_id
 
   read SERVICE_ID_SELECTED
 
   if [[ ! $SERVICE_ID_SELECTED =~ ^[0-5]$ ]]
-  then
-    MAIN_MENU
+  then 
+      MAIN_MENU
   else 
+      
+      # ask for phone number
+      echo -e "\nEnter your phone number"
 
-    echo -e "\nEnter your phone number"
-    read CUSTOMER_PHONE
-    
-    if [[ -z ]]
+      read CUSTOMER_PHONE
 
-    echo -e "\nEnter your name"
-    read CUSTOMER_NAME
+      # check if phone number exist
 
-    echo -e "\nEnter time to be scheduled"
-    read SERVICE_TIME
+      CHECK_PHONE_EXIST=$($PSQL "SELECT customer_id FROM customers WHERE phone='$CUSTOMER_PHONE'")
 
+     if [[ -z $CHECK_PHONE_EXIST ]]
+     then
+        echo "phone doesn't exist"
+      else 
+        echo "phone exist"
+     fi
+
+     # read CUSTOMER_NAME
+
+     # read SERVICE_TIME
   fi
+
 
 }
 
